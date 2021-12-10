4bd76c1878e630b1d3193f266b18aaed065cd27c
DeskSoul29 <jhannavarro@gmail.com>
2021-12-09 19:41:50 -0500

    Proyecto Terminado

--- README.md
+++
@@ -1,2 +0,0 @@
-# ArkhamSupermarket
-Proyecto Final - Ing del Software - 2021_2
---
+++ Sprint3/Jhan Ramirez/ArkhamStore/vistas/modulos/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2021 Ingenieria del Software I - Andrea Camargo, Jhan Ramirez, Diego Echeverria, Julian Gafaro </a>.</strong>
+
+	Todos los derechos reservados.
+
+
+</footer>
